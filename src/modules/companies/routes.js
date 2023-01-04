8b4c9f92a4ce5afc25f02b83013669dc8444fa09
simlenodir <nodirsmailov0@gmail.com>
2023-01-04 13:37:22 +0300

    added pagination

--- src/modules/companies/routes.js
+++ src/modules/companies/routes.js
@@ -6,6 +6,6 @@ const companiesRouter = Router()
 
 export default companiesRouter
 .get('/companies', companies.GET_COMPANIES)
-.post('/create-company', verifyToken, companies.CREATE_COMPANIES)
-.put('/update-company/:compId', verifyToken, companies.UPDATE_COMPANY)
-.delete('/delete-company/:compId', verifyToken, companies.DELETE_COMPANY)
+.post('/create-company', companies.CREATE_COMPANIES)
+.put('/update-company/:compId', companies.UPDATE_COMPANY)
+.delete('/delete-company/:compId', companies.DELETE_COMPANY)
